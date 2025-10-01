9b95cc20c01a0087a77cc746ce482835658a817d
bhargi129 <bhargi129@gmail.com>
2025-10-01 18:01:02 +0530

    03. done

--- 03.Variables.sh
+++ 03.Variables.sh
@@ -3,7 +3,7 @@
 $Person1=Lotta
 $Person2=Motta
 
-echo "Person1 :: Hi Person2, How are you man?"
-echo "Person2 :: Hi Person1, Im doing good man"
-echo "Person1 :: whats up, Person2? what are you doing these amny days"
-echo "Person2 :: All good Person1"
+echo "$Person1 :: Hi $Person2, How are you man?"
+echo "$Person2 :: Hi $Person1, Im doing good man"
+echo "$Person1 :: whats up, $Person2? what are you doing these amny days"
+echo "$Person2 :: All good $Person1"
