f9b1bf2650d54bc0fd609d3a6dc64f703807a07c
bhargi129 <bhargi129@gmail.com>
2025-10-01 17:59:39 +0530

    03.changes done

---
+++ 03.Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$Person1=Lotta
+$Person2=Motta
+
+echo "Person1 :: Hi Person2, How are you man?"
+echo "Person2 :: Hi Person1, Im doing good man"
+echo "Person1 :: whats up, Person2? what are you doing these amny days"
+echo "Person2 :: All good Person1"
